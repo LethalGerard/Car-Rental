3abcb213d0a196aa2720586265301127eb68335d
jakob altstadt <jakob.altstadt@iths.se>
2023-10-30 11:03:24 +0100

    Ibooking och boking kompletering + ändringar

--- Car Rental.Common/Classes/Booking.cs
+++ Car Rental.Common/Classes/Booking.cs
@@ -1,6 +1,16 @@
-﻿namespace Car_Rental.Common.Classes;
+﻿using Car_Rental.Common.Enums;
+using Car_Rental.Common.Interfaces;
 
-internal class Booking
+namespace Car_Rental.Common.Classes;
+
+public class Booking : IBooking
 {
-
+    public IVehicle Vehicle { get; }
+    public IPerson Customer { get; }
+    public int KmRented { get; }
+    public int? KmReturned { get; }
+    public DateTime PickupDate { get; }
+    public DateTime ReturnDate { get; set; }
+    public double? TotalCost { get; set; }
+    public bool Status { get; set; }
 }
--- Car Rental.Common/Interfaces/IBooking.cs
+++ Car Rental.Common/Interfaces/IBooking.cs
@@ -1,6 +1,15 @@
-﻿namespace Car_Rental.Common.Interfaces;
+﻿using Car_Rental.Common.Enums;
+
+namespace Car_Rental.Common.Interfaces;
 
 public interface IBooking
 {
-
+    IVehicle Vehicle { get; }
+    IPerson Customer { get; }
+    int KmRented { get; }
+    int? KmReturned { get; }
+    DateTime PickupDate { get; }
+    DateTime ReturnDate { get; set; }
+    double? TotalCost { get; }
+    bool Status { get; set; }
 }
